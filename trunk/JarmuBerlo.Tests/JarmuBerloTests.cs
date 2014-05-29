2b6f77b6c4f7a5540cd8bf3fd9c6c174e8a7a314
Sanyee04@gmail.com <Sanyee04@gmail.com@e4d1693b-8f33-b3b7-9425-ed22e7ec3f4b>
2014-05-29 11:03:18 +0000

    Test project created.

---
+++ trunk/JarmuBerlo.Tests/JarmuBerloTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using JarmuBerloDAL;
+
+namespace JarmuBerlo.Tests
+{
+    [TestFixture]
+    public class JarmuBerloTests
+    {
+        [Test]
+        public void UserLogin_AdminLogin_Test()
+        {
+            Users users = new Users();
+            int ret = users.Login("admin", Encrypter.Encrypt("admin"));
+            Assert.AreEqual(ret, 0);
+        }
+    }
+}
